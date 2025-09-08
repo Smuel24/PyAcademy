24649d4cdc69a31253c061997a269c538fb6ee48
Samuel Molina <samumoe2401@gmail.com>
2025-09-08 11:22:05 -0500

    corrección de modelos con importacion de user de Django

--- app/models.py
+++ app/models.py
@@ -2,56 +2,53 @@ from django.db import models
 import uuid
 from django.contrib.auth.models import User
 
-# Create your models here.
 class Category(models.Model):
-    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable= False)
-    name = models.CharField(max_length = 150)
-    description = models.CharField(max_length = 300)
-    slug = models.SlugField(unique = True)
-
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=300)
+    slug = models.SlugField(unique=True)
 
 class Course(models.Model):
-    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
-    title = models.CharField(max_length = 250)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=250)
     date = models.DateField()
     duration = models.IntegerField()
-    certification_price = models.DecimalField()
-    slug = models.SlugField(unique = True)
+    certification_price = models.DecimalField(max_digits=8, decimal_places=2)
+    slug = models.SlugField(unique=True)
     img = models.ImageField(upload_to='portadas/', null=True, blank=True)
-    Category = models.ForeignKey(Category, on_delete = models.CASCADE, related_name= 'cursos')
-    Level = (
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='cursos')
+    LEVELS = (
         ('Basic', 'Basic'), 
         ('Intermediate', 'Intermediate'),
         ('Experienced', 'Experienced'), 
-
     )
+    level = models.CharField(max_length=15, choices=LEVELS)
 
 class Module(models.Model):
-    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
-    title = models.CharField(max_length = 200)
-    decripcion = models.CharField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=300)
     order = models.IntegerField()
-
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='modules')
 
 class Resource(models.Model):
-    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
-    title = models.CharField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
     url = models.URLField()
-    duration = models.FloatField(nuul = True, blank = True)
-    module = models.ForeignKey(Module, on_delete=models.CASCADE, related_name='recursos')
+    duration = models.FloatField(null=True, blank=True)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE, related_name='resources')
     TYPES = (
         ('Video', 'Video'),
-        ('PDf', 'PDF'),
+        ('PDF', 'PDF'),
     )
-    type = models.CharField(max_length=10, choices= TYPES)
+    type = models.CharField(max_length=10, choices=TYPES)
 
 class Progreso(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     curso = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='progresos')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='progresos')
     porcentaje = models.FloatField()
     fecha_actualizacion = models.DateTimeField(auto_now=True)
-
-
 
 class Payment(models.Model):
     STATES = (
@@ -59,35 +56,27 @@ class Payment(models.Model):
         ('PAID', 'PAID'),
         ('FAILED', 'FAILED'),
     )
-    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     curso = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='pagos')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pagos')
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     date = models.DateTimeField()
-    transaction_id = models.CharField(max_length = 100)
-    state = models.CharField(max_length = 10, choices = STATES)
-
+    transaction_id = models.CharField(max_length=100)
+    state = models.CharField(max_length=10, choices=STATES)
 
 class Certification(models.Model):
-    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
-    Payment = models.ForeignKey(Payment, on_delete=models.CASCADE, related_name='certificados')
-    unique_code = models.CharField(max_length = 50, unique = True) 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, related_name='certificados')
+    unique_code = models.CharField(max_length=50, unique=True) 
     emition_date = models.DateTimeField()
-
-class User(models.Models):
-    id = models.UUIDField(primary_key = True, default = uuid.uuid, editable = False)
-    mail = models.EmailField(unique = True)
-    password = models.CharField(max_length=128)
-    registration_date = models.DateField()
-    active = models.BooleanField(default = True)
 
 class Profile(models.Model):
     ROLES = (
         ('Estudent', 'Estudent'),
         ('Instructor', 'Instructor'),
-        ('Admin', 'admin'),
+        ('Admin', 'Admin'),
     )
-
-    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
-    name = models.CharField(max_length = 50)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     rol = models.CharField(max_length=20, choices=ROLES)
