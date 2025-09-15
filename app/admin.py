9c86013ca94aa85b84a5653e1f0ffa0356634df3
Smuel24 <samumoe2401@gmail.com>
2025-09-15 09:44:16 -0500

    admin

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
+from .models import Course
+
 
 
 # Register your models here.
+admin.site.register(Course)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'level', 'category', 'certiication_price')
+    search_field = ('title')
+    list_filter = ('level', 'category')
+
+admin.site.register(Course, CourseAdmin)  
