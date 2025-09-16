e2070df6e716d0867d0be679e541040bb783932a
Smuel24 <samumoe2401@gmail.com>
2025-09-16 00:22:11 -0500

    Vista de cursos y categorias

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import HomePageView, CoursesPageView, CategoriesPageView, CartPageView, ProfilePageView, CourseIndexView
+from .views import HomePageView, CoursesPageView, CategoryListView, CartPageView, ProfilePageView, CourseShowView, CategoryDetailView, CartRemoveAllView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('Courses/', CoursesPageView.as_view(), name='cursos'),
-    path('categorias/', CategoriesPageView.as_view(), name='categories'),
-    path('Carrito/', CartPageView.as_view(), name = 'cart'),
-    path('Mi Cuenta/', ProfilePageView.as_view(), name = 'profile'),
-    path('courses/', CourseIndexView.as_view(), name='course_index'),
-
+    path('categorias/', CategoryListView.as_view(), name='categories'),  
+    path('Carrito/', CartPageView.as_view(), name='cart'),
+    path('Mi Cuenta/', ProfilePageView.as_view(), name='profile'),
+    path('curso/<slug:slug>/', CourseShowView.as_view(), name='course_detail'),
+    path('categorias/<slug:slug>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('Carrito/add/<str:course_id>', CartPageView.as_view(), name = 'cart_add'),
+    path('Carrito/removeAll', CartRemoveAllView.as_view(), name = 'cart_removeAll'),
 ]
--- app/views.py
+++ app/views.py
@@ -9,50 +9,72 @@ class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
 
-class CoursesPageView(TemplateView):
-    template_name = 'pages/courses.html'
-
-
 class CategoriesPageView(TemplateView):
     template_name =  'pages/categories.html'
 
 
-class CartPageView(TemplateView):
+class CartPageView(View):
     template_name = 'pages/cart.html'
+
+    def get(self, request):
+        cart_courses = {}
+        cart_courses_data = request.session.get('cart_courses_data', {})
+
+        for key, course in Course.objects.in_bulk(cart_courses_data.keys()).items():
+            cart_courses[key] = course
+
+        context = {
+            'title': 'Carrito de compras',
+            'cart_courses': cart_courses,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, course_id):
+        cart_courses_data = request.session.get('cart_courses_data', {})  # <--- usa el mismo nombre
+        cart_courses_data[str(course_id)] = course_id
+        request.session['cart_courses_data'] = cart_courses_data  # <--- usa el mismo nombre
+
+        return redirect('cart')    
+
+
+class CartRemoveAllView(View):
+    def post(self, request):
+        if 'cart_courses_data' in request.session:
+            del request.session['cart_courses_data']
+
+        return redirect('cart')
 
 
 class ProfilePageView(TemplateView):
     template_name = 'pages/Profile.html'
 
     
-class CourseIndexView(View):
-    template_name = 'course/indexcourse.html'
 
-    def get(self, request):
-        viewData = {}
-        viewData['title'] = "Todos los Cursos"
-        viewData['courses'] = Course.objects.all()
-        return render(request, self.template_name, viewData)
+    
+
+class CoursesPageView(TemplateView):
+    template_name = 'pages/courses.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['courses'] = Course.objects.all()
+        context['title'] = "Todos los cursos"
+        context['query'] = ""
+        return context
 
 
+class CourseShowView(View):
+    template_name = 'course/showcourse.html'
 
-class CoureShowView(View):
-    Template_name = 'course/showcourse.html'
-
-    def get(self, request): 
-
-            try:
-                course_id = int(id)
-                if course_id < 1:
-                    raise ValueError("course id must be 1 or greater")
-                course = get_object_or_404(Course, pk = course_id)
-            except(ValueError, IndexError):
-                return HttpResponseRedirect
+    def get(self, request, slug):
+        course = get_object_or_404(Course, slug=slug)
+        context = {'course': course}
+        return render(request, self.template_name, context)
                 
                 
 
 class CategoryListView(View):
-    template_name = 'category/categories.html'
+    template_name = 'pages/categories.html'  
 
     def get(self, request):
         search_query = request.GET.get('q', '')  # Agrega el parámetro de búsqueda
@@ -71,17 +93,15 @@ class CategoryListView(View):
         }
         return render(request, self.template_name, context)
 
+
 class CategoryDetailView(View):
     template_name = 'category/category_detail.html'
-
     def get(self, request, slug):
         category = get_object_or_404(Category, slug=slug)
-        # Los cursos de la categoría se acceden por: category.cursos.all()
-        courses = category.cursos.all()
+        # Si tienes la relación, úsala así:
+        # courses = category.courses.all()
         context = {
             'category': category,
-            'courses': courses,
-            'title': f'Categoría: {category.name}'
+            # 'courses': courses, # descomenta si tienes la relación
+            'title': f'Categoría: {category.name}',
         }
-        return render(request, self.template_name, context)
-            
